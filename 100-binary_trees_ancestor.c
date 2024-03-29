b3580f206b82c5e8f6735b6b1467f56de134ce8f
Iyanu Dayisi <tobiy23@gmail.com>
2024-03-29 12:08:38 +0100

    find our ancestors

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor for both
+ *			nodes.
+ * @first: the first node.
+ * @second: the second node.
+ *
+ * Return: a pointer to the ancestor
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									const binary_tree_t *second)
+{
+	if ((first == NULL) || (second == NULL))
+		return (NULL);
+	return (find_ancestor(first, second));
+}
+/**
+ * find_ancestor - finds the common ancestor
+ *					-> if a->parent == b->parent => a->parent
+ *					-> if a == b->parent => a
+ *					-> if a->parent == b => b
+ * @firstNode: the first Node
+ * @secondNode: the econd Node
+ *
+ * Return: the ancestor
+ */
+
+binary_tree_t *find_ancestor(const binary_tree_t *firstNode,
+							const binary_tree_t *secondNode)
+{
+	if (firstNode == secondNode)
+	{
+		return ((binary_tree_t *)firstNode);
+	}
+
+	if (firstNode->parent == NULL || firstNode == secondNode->parent)
+	{
+		return (find_ancestor(firstNode, secondNode->parent));
+	}
+
+	if (secondNode->parent == NULL || secondNode == firstNode->parent)
+	{
+		return (find_ancestor(secondNode, firstNode->parent));
+	}
+
+	return (find_ancestor(firstNode->parent, secondNode->parent));
+
+}
